577caad155467e1def4a69fef8c764351a731cf4
DevHyundo <101845928+DevHyundo@users.noreply.github.com>
2022-11-14 12:27:59 +0900

    Update BController.java

--- src/main/java/com/example/ignoretest/controller/BController.java
+++ src/main/java/com/example/ignoretest/controller/BController.java
@@ -3,5 +3,6 @@ package com.example.ignoretest.controller;
 public class BController {
     void print(){
         System.out.println("BController.print");
+        System.out.println("someone fix here");
     }
 }
