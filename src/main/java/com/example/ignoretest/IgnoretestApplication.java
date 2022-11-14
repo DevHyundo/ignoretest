eabbc70067c50e714b84413e5e92f71e3c956aaf
DevHyundo <08hyundo@gmail.com>
2022-11-14 11:46:23 +0900

    first commit

---
+++ src/main/java/com/example/ignoretest/IgnoretestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ignoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoretestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgnoretestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ignoretest/controller/AController.java
@@ -0,0 +1,8 @@
+package com.example.ignoretest.controller;
+
+public class AController {
+
+    void aaa(){
+        System.out.println("AController.aaa");
+    }
+}
